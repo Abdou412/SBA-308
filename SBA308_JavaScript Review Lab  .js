3279e22c02d332141c65fe5711aeb334669ca1c8
mohamed abdallahi Elhadramy <abdoullahhadramy@gmail.com>
2024-12-03 11:21:02 -0500

    section 1 A (Q+A)

---
+++ SBA308_JavaScript Review Lab  .js
@@ -0,0 +1,42 @@
+// Section 1
+// JavaScript Total Recall
+
+//A. Q + A
+                            *****************
+// How do we assign a value to a variable?
+// to assign a value to a variable we use the assignment operator =
+// exemple let A=2, the variable A takes the value 2.
+
+                           ******************
+
+// How do we change the value of a variable?
+
+// to change the vavalue of a variable we  simply reassign the new value to the variable, as js takes always the last
+// value of a variable. eg A = 7; now we have changed the value of the variable A to 7.
+//
+                   ******************************
+//
+// How do we assign an existing variable to a new variable?
+// to assign an existing variable to a new variabl 
+// we give the first variable as value to the last varible.
+//  eg: let x = 7;
+//       let y = x ; // now y ==x;
+
+                       **********************************
+// Remind me, what are declare, assign, and define?
+//    
+//    declare a variable is like reserve a place for that variable in the storage.
+//   assign a vvariable is to link that variable to a specific value.
+//    define a variable is to make it recognizable by the programming languange.
+
+                     ********************************
+//      What is pseudocoding and why should you do it?
+// 
+//       i would like to say that pseudocoding is a list of steps that we follow to reach the result that we want, 
+        // it is necessary to build a propre programe and avoid errors and problems during the process operation. 
+                        ***********************************
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+            // honstly,i do not know, but after some researches on google, it turn out 
+            // that it is a common guide line to spend 70-80% of time thinking and plannig 
+            // how to solve the problem and the rest in coding.
+            
